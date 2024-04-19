622e81419b15827a64a1d93646521d73958e701f
piyush15081999 <157483801+piyush15081999@users.noreply.github.com>
2024-04-19 23:16:43 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,5 +3,6 @@
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/clothing/requirements.txt
+pip install pillow
 sudo chmod 777 /home/ubuntu/clothing
 sudo chmod 777 /home/ubuntu/clothing/db.sqlite3
