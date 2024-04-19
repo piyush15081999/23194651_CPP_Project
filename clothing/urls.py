1d3b168fb3563ef53ccd12dc06c6ef69f2cb5b9b
piyush15081999 <157483801+piyush15081999@users.noreply.github.com>
2024-04-19 18:09:15 +0530

    Add files via upload

---
+++ clothing/urls.py
@@ -0,0 +1,45 @@
+"""
+URL configuration for clothing project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.loginx, name='loginx'),
+    path('signup/', views.signup, name='signup'),
+    path('handle_login/', views.handle_login, name='handle_login'),
+    path('handle_signup/', views.handle_signup, name='handle_signup'),
+    path('all_products/', views.all_products, name='all_products'),
+    path('category_item/', views.category_item, name='category_item'),
+    path('product_detail/', views.product_detail, name='product_detail'),
+    path('add_cart/', views.add_cart, name='add_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('handle_checkout/', views.handle_checkout, name='handle_checkout'),
+    path('order_item/', views.order_item, name='order_item'),
+    path('logout/', views.logoutx, name='logoutx'),
+]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ clothing/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render, redirect
+from product.models import Category, Subcategory, Prod
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from orderapp.models import Cartitem, Order
+import datetime
+from django.http import HttpResponse
+
+def index(request):
+    p= Prod.objects.all()
+    cat = Subcategory.objects.all()
+    return render(request, 'index.html', {'p':p, 'cat':cat})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def loginx(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'register.html')    
+
+def handle_login(request):
+    if request.method == 'POST':
+        uname = request.POST.get("uname")
+        pass1 = request.POST.get("pass")
+        user = authenticate(username=uname, password=pass1)
+        if user is not None:
+            login(request, user)
+
+            # messages.success(request, "Logged in")
+            return redirect('home')
+        else:
+            # messages.info(request,'invalid credentials')
+            return redirect('loginx')  
+    return redirect('home')                   
+
+def handle_signup(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        uname = request.POST.get("uname")
+        email = request.POST.get("email")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+
+        if pass1==pass2:
+
+
+            myuser = User.objects.create_user(username=uname, email=email, password=pass1,)
+            myuser.first_name = name
+            myuser.save()
+            # messages.success(request, "Your account has been created")
+            print('success')
+            return redirect("home")
+    else:
+
+        return HttpResponse("404 error") 
+
+def all_products(request):
+    p= Prod.objects.all()
+    cat= Subcategory.objects.all()
+    return render(request, 'products.html', {'p':p, 'cat':cat})
+
+def category_item(request):
+    id = request.POST.get("id")
+    c=Subcategory.objects.get(pk=id)
+    p= Prod.objects.filter(subcat_id=id)
+    cat= Subcategory.objects.all()
+    return render(request, 'category_item.html', {'p':p, 'cat':cat, 'c':c})    
+
+def product_detail(request):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        p= Prod.objects.get(pk=id)
+        cat= Subcategory.objects.all()
+        print(p)
+        return render(request, 'product_detail.html', {'p':p, 'cat':cat})    
+    else:
+
+        return HttpResponse("404 error")    
+
+def add_cart(request):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        p= Prod.objects.get(pk=id)
+        c= Cartitem(prod_id=p.pk, quantity=1, user_id=request.user.pk)
+        c.save()
+        print(p)
+        return redirect('home')    
+    else:
+
+        return HttpResponse("Login TO Add Cart")         
+
+def checkout(request):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        p= Prod.objects.get(pk=id)
+        c= Cartitem.objects.filter(user_id=request.user.pk)
+        t=p.prod_price
+        for item in c:
+            t=t+item.quantity*item.prod.prod_price
+        print(p)
+        return render(request, 'checkout.html', {'p':p, 'c':c, 't':t})    
+    else:
+
+        return HttpResponse("Login TO Buy")         
+
+def handle_checkout(request):
+    if request.method == 'POST':
+        prod_id1 = request.POST.get("prod_id")
+        p= Prod.objects.get(pk=prod_id1)
+        print(p)
+        c= Cartitem.objects.filter(user_id=request.user.pk)
+        
+        now = datetime.datetime.now()
+        date_string = now.strftime("%Y%m%d%H%M%S")
+        date_int = int(date_string)
+        o1= Order(prod_id=p.pk, quantity=1, user_id=request.user.pk, order_main_id=date_int)
+        
+        for item in c:
+            print(item.prod)
+            o=Order(prod_id=item.prod_id, quantity=item.quantity, user_id=request.user.pk, order_main_id=date_int)
+            o.save()
+        # c= Cartitem(prod_id=p.pk, quantity=1, user_id=request.user.pk)
+        # c.save()
+        o1.save()
+        print(p)
+        return redirect('home')    
+    else:
+
+        return HttpResponse("Login To Order")         
+
+def order_item(request):
+    if request.user.is_authenticated:
+        o= Order.objects.filter(user_id=request.user.pk)
+        # cat= Subcategory.objects.all()
+        return render(request, 'order_item.html', {'o':o})
+    else:
+        return HttpResponse("Login To Check Items")    
+
+def logoutx(request):
+    logout(request)
+    return redirect('home')        
