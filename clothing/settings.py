2a73859c194d412e42eab47e8d5ece9e8407431a
piyush15081999 <157483801+piyush15081999@users.noreply.github.com>
2024-04-20 21:40:23 +0530

    Update settings.py

--- clothing/settings.py
+++ clothing/settings.py
@@ -149,4 +149,4 @@ AWS_S3_REGION_NAME = os.environ.get('eu-west-1')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = os.environ.get('storages.backends.s3boto3.S3Boto3Storage')
+# DEFAULT_FILE_STORAGE = os.environ.get('storages.backends.s3boto3.S3Boto3Storage')
