65070c292a176b48980108b131674c9a5a43f083
piyush15081999 <157483801+piyush15081999@users.noreply.github.com>
2024-04-20 14:28:47 +0530

    Update models.py

--- product/models.py
+++ product/models.py
@@ -12,6 +12,6 @@ class Subcategory(models.Model):
 class Prod(models.Model):
     prod_name= models.CharField(max_length= 50)
     subcat= models.ForeignKey(Subcategory, on_delete=models.PROTECT)
-    prod_image = models.ImageField(upload_to='images')
+    prod_image = models.ImageField(upload_to='media/images')
     prod_desc= models.TextField()
     prod_price= models.FloatField(null=True, blank=True, default=None)
